988e0393a9544018b0d2369030ee2f4b5606ac0f
Aditya Raj Singh <140361037+sadityaraj861@users.noreply.github.com>
2025-08-14 16:10:40 +0530

    Time: 0 ms (100%), Space: 41.1 MB (73.93%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -10,26 +10,16 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int size=0;
-        boolean flag=true;//when size of the list is odd.
-        ListNode temp=head;
-        while(temp!=null){
-            size++;
-            temp=temp.next;
-        }
-        temp=head;
+        ListNode slow=head;
+        ListNode fast=head;
 
-
-        if(flag){
-            for(int i=0;i<size/2;i++){
-                temp=temp.next;
-            }
-            return temp;
-        }else{
-            for(int i=0;i<(size/2)+1;i++){
-                temp=temp.next;
-            }
-            return temp;
+// for odd-  while(fast.next!=null)
+//for even - while(fast!=null)
+      //merging the both condition for odd and even 
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
+        return slow;
     }
 }
