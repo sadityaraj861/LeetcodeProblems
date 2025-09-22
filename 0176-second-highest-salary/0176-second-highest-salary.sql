686d095551a63baa9ecc552beaafe6502d844171
Aditya Raj Singh <140361037+sadityaraj861@users.noreply.github.com>
2025-09-23 01:26:32 +0530

    Time: 286 ms (33.66%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,8 @@
+
+#Direct SELECT ... FROM ... WHERE ... → empty set if no matches.
+#Scalar subquery SELECT (SELECT ... ) → returns one row with NULL if no matches.
+#This is why the “nested in SELECT” trick is so popular for these “Nth highest” problems.
+
+select(
+    select Distinct salary from employee order by salary desc limit 1 offset 1
+)As SecondHighestSalary;
