cbb8a7d339891380fb73334d398fc3b58c0fe95d
Aditya Raj Singh <140361037+sadityaraj861@users.noreply.github.com>
2025-09-23 01:29:10 +0530

    Time: 264 ms (64.2%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -3,6 +3,11 @@
 #Scalar subquery SELECT (SELECT ... ) → returns one row with NULL if no matches.
 #This is why the “nested in SELECT” trick is so popular for these “Nth highest” problems.
 
-select(
-    select Distinct salary from employee order by salary desc limit 1 offset 1
-)As SecondHighestSalary;
+-- select(
+--     select Distinct salary from employee order by salary desc limit 1 offset 1
+-- )As SecondHighestSalary;
+
+#or
+
+select max(salary) as SecondHighestSalary from Employee where salary<(select max(salary) from Employee);
+
